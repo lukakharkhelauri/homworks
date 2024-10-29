2c4eacde314961549cd8bd301c0a52172beb6230
lukakharkhelauri <148798486+lukakharkhelauri@users.noreply.github.com>
2024-10-29 12:13:51 +0400

    Add files via upload

---
+++ loop-homework-2/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //exercise 1
+//        int n = 10;
+//        int sum = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            sum += i;
+//        }
+//        double average = (double) sum / n;
+//        System.out.println(average);
+
+            //exercise 3
+//        for (int i = 0; i < 15; i++) {
+//            if (i == 5 || i == 7 || i == 11) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        //exercise 4
+//        int binary = 1101;
+//        int num1 = 0;
+//        int num2 = 1;
+//
+//        while (binary > 0) {
+//            int lastDigit = binary % 10;
+//            num1 += lastDigit * num2;
+//            binary /= 10;
+//            num2 *= 2;
+//        }
+//        System.out.println(num1);
+
+        //exercise 5
+//        int num1 = 12;
+//        String binary = "";
+//
+//        while (num1 > 0) {
+//            int num2 = num1 % 2;
+//            binary = num2 + binary;
+//            num1 /= 2;
+//        }
+//        System.out.println(binary);
+
+
+        //exercise 6
+//        Random random = new Random();
+//        int x = random.nextInt(100) + 1;
+//
+//        Scanner myObj = new Scanner(System.in);
+//        System.out.println("Enter digit to guess \uD83E\uDD84");
+//        int digit = myObj.nextInt();
+//
+//        while (digit != x) {
+//            System.out.println("Enter digit to guess \uD83E\uDD84");
+//            digit = myObj.nextInt();
+//            if (digit > x) {
+//                System.out.println("Your digit is more than x");
+//            } else if (digit < x) {
+//                System.out.println("Your digit is less than x");
+//            } else {
+//                System.out.println("congrats! Your digit equals x \uD83C\uDF89");
+//            }
+//        }
+//
+//        myObj.close();
+    }
+}
